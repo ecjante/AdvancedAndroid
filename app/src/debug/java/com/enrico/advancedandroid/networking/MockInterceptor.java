69cf4c632f0cd9d5a5aad32b4d227d8442433317
Enrico Jante <enrico.jante@gmail.com>
2018-03-14 15:21:18 -0700

    Add debug interceptor

---
+++ app/src/debug/java/com/enrico/advancedandroid/networking/MockInterceptor.java
@@ -0,0 +1,46 @@
+package com.enrico.advancedandroid.networking;
+
+import com.enrico.advancedandroid.settings.DebugPreferences;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * Created by enrico on 3/14/18.
+ */
+
+public class MockInterceptor implements Interceptor {
+
+    private final MockResponseFactory mockResponseFactory;
+    private final DebugPreferences debugPreferences;
+
+    @Inject
+    MockInterceptor(MockResponseFactory mockResponseFactory, DebugPreferences debugPreferences) {
+        this.mockResponseFactory = mockResponseFactory;
+        this.debugPreferences = debugPreferences;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        if (debugPreferences.useMockResponsesEnabled()) {
+            String mockResponse = mockResponseFactory.getMockResponse(chain.request());
+            if (mockResponse != null) {
+                return new Response.Builder()
+                        .message("")
+                        .protocol(Protocol.HTTP_1_1)
+                        .request(chain.request())
+                        .code(200)
+                        .body(ResponseBody.create(MediaType.parse("text/json"), mockResponse))
+                        .build();
+            }
+        }
+        return chain.proceed(chain.request());
+    }
+}
